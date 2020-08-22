eda126b2775a18bb04880953b3efb2ee91b318bc
Dillon Kearns <dillon@dillonkearns.com>
2020-08-21 23:25:47 -0700

    Extract module.

--- src/Ical.elm
+++ src/Ical.elm
@@ -1,5 +1,54 @@
-module Ical exposing (dummy)
+module Ical exposing (generate)
+
+import Format
+import Iso8601
+import Rfc3339
+import Time
 
 
-dummy =
-    ()
+type alias Event =
+    { stamp : Time.Posix
+    , start : Time.Posix
+    , end : Time.Posix
+    , summary : String
+    , id : String
+    }
+
+
+eventGenerate : Config -> Event -> String
+eventGenerate config details =
+    """BEGIN:VEVENT
+"""
+        ++ formatKeys (keys config details)
+        ++ """
+END:VEVENT"""
+
+
+keys : Config -> Event -> List ( String, String )
+keys config details =
+    [ ( "UID", details.id ++ "@" ++ config.domain )
+    , ( "DTSTAMP", details.stamp |> Rfc3339.format ) -- https://www.kanzaki.com/docs/ical/dtstamp.html
+    , ( "DTSTART", details.start |> Rfc3339.format )
+    , ( "DTEND", details.end |> Rfc3339.format )
+    , ( "SUMMARY", details.summary )
+    ]
+
+
+formatKeys : List ( String, String ) -> String
+formatKeys nodes =
+    nodes
+        |> List.map Format.normalizeField
+        |> String.join "\n"
+
+
+type alias Config =
+    { id : String, domain : String }
+
+
+generate : Config -> List Event -> String
+generate config events =
+    """BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//incrementalelm.com//elm-ical.tests//EN
+""" ++ String.join "\n" (List.map (eventGenerate config) events) ++ """
+END:VCALENDAR"""
--- tests/FeedTests.elm
+++ tests/FeedTests.elm
@@ -1,59 +1,10 @@
 module FeedTests exposing (..)
 
 import Expect exposing (Expectation)
-import Format
+import Ical
 import Iso8601
-import Rfc3339
 import Test exposing (..)
 import Time
-
-
-type alias Event =
-    { stamp : Time.Posix
-    , start : Time.Posix
-    , end : Time.Posix
-    , summary : String
-    , id : String
-    }
-
-
-eventGenerate : Config -> Event -> String
-eventGenerate config details =
-    """BEGIN:VEVENT
-"""
-        ++ formatKeys (keys config details)
-        ++ """
-END:VEVENT"""
-
-
-keys : Config -> Event -> List ( String, String )
-keys config details =
-    [ ( "UID", details.id ++ "@" ++ config.domain )
-    , ( "DTSTAMP", details.stamp |> Rfc3339.format ) -- https://www.kanzaki.com/docs/ical/dtstamp.html
-    , ( "DTSTART", details.start |> Rfc3339.format )
-    , ( "DTEND", details.end |> Rfc3339.format )
-    , ( "SUMMARY", details.summary )
-    ]
-
-
-formatKeys : List ( String, String ) -> String
-formatKeys nodes =
-    nodes
-        |> List.map Format.normalizeField
-        |> String.join "\n"
-
-
-type alias Config =
-    { id : String, domain : String }
-
-
-generateFeed : Config -> List Event -> String
-generateFeed config events =
-    """BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//incrementalelm.com//elm-ical.tests//EN
-""" ++ String.join "\n" (List.map (eventGenerate config) events) ++ """
-END:VCALENDAR"""
 
 
 suite : Test
@@ -74,7 +25,7 @@ suite =
                   , summary = "repeating by day, twice"
                   }
                 ]
-                    |> generateFeed { id = "//incrementalelm.com//elm-ical.tests//EN", domain = "incrementalelm.com" }
+                    |> Ical.generate { id = "//incrementalelm.com//elm-ical.tests//EN", domain = "incrementalelm.com" }
                     |> expectEqualLines """BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//incrementalelm.com//elm-ical.tests//EN
